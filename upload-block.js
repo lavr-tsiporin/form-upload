b721eb5c73374a5d2e6af733fc7990ebadc64715
Lavr <avatar_str@list.ru>
2022-02-13 16:42:49 +0200

    Рефакторинг кода. Разделение. Добавление дополнительных полей

--- upload-block.js
+++ upload-block.js
@@ -1,20 +1,79 @@
-const DELAY_BLOCK = 10000
+const DELAY_BLOCK = 10 //s
 let result = []
 
-const createUpload = (root) => {
-  const span = document.createElement('span')
-  const input = document.createElement('input')
-  const button = document.createElement('button')
-  root.before(span)
-  span.append(input, button)
-  span.setAttribute('style', `margin: 15px 0; display: flex; align-items: center; justify-content: end;`)
-  input.setAttribute('type', 'file')
-  input.setAttribute('accept', '.csv')
-  input.setAttribute('id', 'upload-block-file')
-  input.addEventListener('change', handlerUploadFile)
-  button.classList.add('btn', 'btn-primary')
-  button.textContent = 'Start'
-  button.addEventListener('click', () => handlerStartBlock(result))
+const htmlElements = {
+  root: document.querySelector('#addBlocking'),
+  license: document.querySelector('input[name="key"]'),
+  description: document.querySelector('textarea[name="description"]'),
+  ticket: document.querySelector('input[name="ticket"]'),
+  tillDate: document.querySelector('input[name="tillDate"]'),
+  btnSubmit: document.querySelector('button[type="submit"]')
+}
+
+function CreateElement({ name, options, contents, events }) {
+  const element = document.createElement(name)
+  if (options !== undefined) {
+    Object.keys(options).forEach((keyOption) => {
+      element.setAttribute(keyOption, options[keyOption])
+    })
+  }
+  if (contents !== undefined) {
+    Object.keys(contents).forEach((keyContent) => {
+      element[keyContent] = contents[keyContent]
+    })
+  }
+  if (events !== undefined) {
+    Object.keys(events).forEach((keyEvent) => {
+      element.addEventListener(keyEvent, events[keyEvent])
+    })
+  }
+  return element
+}
+
+function createUpload(root) {
+  const spanElement = CreateElement({
+    name: 'span',
+    options: {
+      style: 'margin: 15px 0; display: flex; align-items: center; justify-content: end;'
+    }
+  })
+  const inputFile = CreateElement({
+    name: 'input',
+    options: {
+      type: 'file',
+      accept: '.csv',
+      id: 'upload-block-file'
+    },
+    events: {
+      change: handlerUploadFile
+    }
+  })
+  const inputTicket = CreateElement({
+    name: 'input',
+    options: {
+      style: 'margin: 0 15px;',
+      placeholder: 'ticket'
+    }
+  })
+  const inputDate = CreateElement({
+    name: 'input',
+    options: {
+      style: 'margin: 0 15px',
+      type: 'date',
+    }
+  })
+  const buttonElement = CreateElement({
+    name: 'button',
+    contents: {
+      className: 'btn btn-primary',
+      textContent: 'Start'
+    },
+    events: {
+      click: () => handlerStartBlock({ result, inputTicket, inputDate })
+    }
+  })
+  root.before(spanElement)
+  spanElement.append(inputFile, inputTicket, inputDate, buttonElement)
 }
 
 function handlerUploadFile() {
@@ -33,7 +92,7 @@ function handlerUploadFile() {
   reader.addEventListener('error', () => window.alert('Ошибка при загрузке файла'))
 }
 
-const formatFileInArray = (str) => {
+function formatFileInArray(str) {
   return str
     .split('@')
     .filter(el => el !== '')
@@ -51,12 +110,13 @@ const formatFileInArray = (str) => {
     }))
 }
 
-async function handlerStartBlock(data) {
-  if (data.length < 1) {
+async function handlerStartBlock({ result, inputTicket, inputDate }) {
+  const { description, ticket, tillDate, btnSubmit, license } = htmlElements
+  if (result.length < 1) {
     return
   }
 
-  for (let i = 0; i < data.length; i++) {
+  for (let i = 0; i < result.length; i++) {
     try {
       await new Promise((resolve) => {
         setTimeout(() => {
@@ -66,31 +126,43 @@ async function handlerStartBlock(data) {
       })
       await new Promise((resolve) => {
         setTimeout(() => {
-          document.querySelector('input[name="key"]').value = data[i].license
+          license.value = result[i].license
           resolve(true)
         }, 1000)
       })
       await new Promise((resolve) => {
         setTimeout(() => {
-          document.querySelector('textarea[name="description"]').value = data[i].resolution
+          description.value = result[i].resolution
           resolve(true)
         }, 1000)
       })
       await new Promise((resolve) => {
         setTimeout(() => {
-          document.querySelector('button[type="submit"]').click()
+          ticket.value = inputTicket.value
+          resolve(true)
+        }, 1000)
+      })
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          tillDate.value = inputDate.value
+          resolve(true)
+        }, 1000)
+      })
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          btnSubmit.click()
           resolve(true)
         }, 1000)
       })
       await new Promise((resolve, reject) => {
         setTimeout(() => {
           const select = document.querySelector('select.form-control')
-          if (select.value === '') {
-            console.log(`${data[i].license} blocks with ${data[i].resolution}`)
-            resolve(true)
+          if (select.value !== '') {
+            reject(result[i].license)
           }
-          reject(data[i].license)
-        }, DELAY_BLOCK)
+          console.log(`${result[i].license} blocks with ${result[i].resolution}`)
+          resolve(true)
+        }, DELAY_BLOCK * 1000)
       })
     } catch (e) {
       console.error(e)
@@ -98,5 +170,4 @@ async function handlerStartBlock(data) {
   }
 }
 
-const root = document.querySelector('#addBlocking')
-createUpload(root)
+createUpload(htmlElements.root)
